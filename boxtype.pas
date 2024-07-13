8ac95649b6d97d5da8c4176d6051e04652be8a8e
Kevin Veroneau <kveroneau@gmail.com>
2024-07-13 15:12:12 -0600

    Added many more system templates.

--- boxtype.pas
+++ boxtype.pas
@@ -25,7 +25,7 @@ type
   end;
 
 var
-  sys_templates: Array[1..2] of TBoxTemplate;
+  sys_templates: Array[1..5] of TBoxTemplate;
 
 implementation
 
@@ -44,6 +44,27 @@ begin
     cfgres:='ASUS386';
     nvres:='ASUS386NVR';
     nvrFile:='asus386.nvr';
+  end;
+  with sys_templates[3] do
+  begin
+    title:='ALI 486';
+    cfgres:='ALI486';
+    nvres:='ALI1429';
+    nvrFile:='ali1429.nvr';
+  end;
+  with sys_templates[4] do
+  begin
+    title:='Pentium';
+    cfgres:='PENTIUM';
+    nvres:='OPTI560L';
+    nvrFile:='opti560l.nvr';
+  end;
+  with sys_templates[5] do
+  begin
+    title:='Aptiva AMD K-6';
+    cfgres:='APTIVA';
+    nvres:='P5A';
+    nvrFile:='p5a.nvr';
   end;
 end;
 
